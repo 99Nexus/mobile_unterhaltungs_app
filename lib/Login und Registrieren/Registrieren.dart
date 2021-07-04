53d91d173a6971d86170455a0a8e27828b0bd8bd
99Nexus <dustin.cziurlok001@stud.fh-dortmund.de>
2021-07-04 22:35:23 +0200

    -Überarbeitung der Register-Seite -Anbindung der Registrierung an die Datenbank

--- lib/Login und Registrieren/Registrieren.dart
+++ lib/Login und Registrieren/Registrieren.dart
@@ -49,13 +49,13 @@ class RegistrierenHomePageState extends State {
   // Asynchrone Funktion um FlutterFire zu initialisieren
   void initializeFlutterFire() async {
     try {
-      // Wait for Firebase to initialize and set `_initialized` state to true
+
       await Firebase.initializeApp();
       setState(() {
         _initialized = true;
       });
     } catch(e) {
-      // Set `_error` state to true if Firebase initialization fails
+
       setState(() {
         _error = true;
       });
